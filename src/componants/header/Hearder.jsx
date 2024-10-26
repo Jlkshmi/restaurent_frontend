4cdd534614eca532092fa09410e33af18a7a6e46
Jlkshmi <jayalakshmibinija2018@gmail.com>
2024-10-26 14:22:33 +0530

    new file added

--- src/componants/header/Hearder.jsx
+++ src/componants/header/Hearder.jsx
@@ -3,8 +3,13 @@ import "./Header.css"
 
 function Hearder() {
   return (
+    <div className='display'>
     <div>
         <img src="\images\bgimage3.jpg" className='bg_image'/>
+    </div>
+    <div className='image-text'>
+        <p>FIND YOUR <br/>HAPPINESS HERE</p>
+    </div>
     </div>
   )
 }
